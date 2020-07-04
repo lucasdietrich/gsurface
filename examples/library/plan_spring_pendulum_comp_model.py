add diff check, fix Ec Ep Em calculation, fix sphere.py

examples/library/plan_spring_pendulum_comp_model.py
@@ -1,6 +1,6 @@
 from surface_guided_sim import SurfaceGuidedMassSystem, SpringForce, LengthedSpringForce, Gravity, AirFriction, ViscousFriction
 
-from surface_guided_sim.surface import Tore
+from surface_guided_sim.surface.plan import Plan
 
 import matplotlib.pyplot as plt
 from mayavi import mlab
@@ -9,34 +9,34 @@ import numpy as np
 
 # config
 
-u0, du0 = 0.0, 0.5
+x0, vx0 = 2.0, 0.0
 
-v0, dv0 = 1.0, 0.0
+y0, vy0 = 0.0, 4.0
 
 # model
 
-tore = Tore(0.5, 1.0)
+plan = Plan.from_xz_rotation(angle=0.0).setlims(-2, 2.5, -3.5, 5).multlims(1)
 
 m = 1.0
 
 system = SurfaceGuidedMassSystem(
-    surface=tore,
-    s0=np.array([u0, du0, v0, dv0]),
+    surface=plan,
+    s0=np.array([x0, vx0, y0, vy0]),
     m=m,
     forces=[
-        SpringForce(stiffness=5.0, clip=np.array([0.0, 0.0, 0.0])),
-        # LengthedSpringForce(stiffness=5.0, clip=np.array([0.0, 0.0, 0.0]), l0=1.0),
-        ViscousFriction(mu=0.4),
+        Gravity(m=m, g=np.array([0.0, 0.0, -9.81])),
+        LengthedSpringForce(stiffness=1.0, clip=np.array([0.0, 0.0, 0.0]), l0=1.0),
+        ViscousFriction(mu=0.15),  # test for mu=0.2 and 0.8
     ]
 )
 
 # simulate
-time = np.linspace(0, 40, 5000)
+time = np.linspace(0, 60, 6000)
 
 data = system.solve(time)
 
 # build (todo opti)
-mesh = tore.buildsurface(*tore.mesh(100, 100))
+mesh = plan.buildsurface(*plan.mesh(100, 100))
 trajectory = system.surface.trajectory(data[:, 0::2])
 speed = system.surface.speed(data)
 abs_speed = np.linalg.norm(speed, 2, 1)
@@ -44,13 +44,11 @@ abs_speed = np.linalg.norm(speed, 2, 1)
 # plot surface & trajectory
 mlab.figure(1, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(800, 800))
 
-mlab.mesh(*mesh, opacity=0.3, colormap='cool')  # , color=(0.1, 0.1, 0.6)
+mlab.mesh(*mesh, opacity=1.0, colormap='binary')  # , color=(0.1, 0.1, 0.6)
 mlab.mesh(*mesh, opacity=0.1, color=(0, 0, 0), representation='wireframe')
 
-mlab.points3d(*trajectory[0], color=(0, 0, 1), scale_factor=0.05)
-
+mlab.points3d(*trajectory[0], color=(0, 0, 1), scale_factor=0.1)
 mlab.points3d(0, 0, 0, color=(0, 1, 0), scale_factor=0.05)
-# mlab.points3d(0, -1, 0, color=(0, 1, 0), scale_factor=0.05)
 
 mlab.plot3d(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2], color=(1, 0, 0), tube_radius=0.01)
 

examples/library/speed_validation.py
@@ -1,45 +1,54 @@
 from surface_guided_sim import SurfaceGuidedMassSystem, SpringForce, LengthedSpringForce, Gravity, AirFriction, ViscousFriction
 
-from surface_guided_sim.surface import Tore
+from surface_guided_sim.surface import Sphere, Plan, Tore
 
 import matplotlib.pyplot as plt
 from mayavi import mlab
 
 import numpy as np
 
-# config
-
-u0, du0 = 0.0, 0.5
-
-v0, dv0 = 1.0, 0.0
-
 # model
 
-tore = Tore(0.5, 1.0)
+sphere = Sphere(1.0)
 
 m = 1.0
+g = np.array([-1.0, 0.0, 0.0])
+k = 1.0
+clip = np.array([0.0, 0.0, 1.0])
 
 system = SurfaceGuidedMassSystem(
-    surface=tore,
-    s0=np.array([u0, du0, v0, dv0]),
+    surface=sphere,
+    s0=np.array([np.pi/2, 1.0, np.pi/2, 0.0]),
     m=m,
     forces=[
-        SpringForce(stiffness=5.0, clip=np.array([0.0, 0.0, 0.0])),
-        # LengthedSpringForce(stiffness=5.0, clip=np.array([0.0, 0.0, 0.0]), l0=1.0),
-        ViscousFriction(mu=0.4),
+        Gravity(m=m, g=g),
+        SpringForce(stiffness=k, clip=clip),
+        # ViscousFriction(mu=0.2),
     ]
 )
 
 # simulate
-time = np.linspace(0, 40, 5000)
+time = np.linspace(0, 10, 5000)
 
 data = system.solve(time)
 
 # build (todo opti)
-mesh = tore.buildsurface(*tore.mesh(100, 100))
+mesh = sphere.buildsurface(*sphere.mesh(50, 50))
 trajectory = system.surface.trajectory(data[:, 0::2])
 speed = system.surface.speed(data)
 abs_speed = np.linalg.norm(speed, 2, 1)
+
+Ec = 0.5 * m * abs_speed**2
+
+Epk = np.zeros_like(Ec)
+for i in range(len(trajectory)):
+    Epk[i] = 0.5*k*np.linalg.norm(trajectory[i] - clip, 2)**2
+
+Epg = - m*trajectory.dot(g)
+
+Ep = Epg + Epk
+Em = Ec + Ep
+
 
 # plot surface & trajectory
 mlab.figure(1, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(800, 800))
@@ -48,9 +57,6 @@ mlab.mesh(*mesh, opacity=0.3, colormap='cool')  # , color=(0.1, 0.1, 0.6)
 mlab.mesh(*mesh, opacity=0.1, color=(0, 0, 0), representation='wireframe')
 
 mlab.points3d(*trajectory[0], color=(0, 0, 1), scale_factor=0.05)
-
-mlab.points3d(0, 0, 0, color=(0, 1, 0), scale_factor=0.05)
-# mlab.points3d(0, -1, 0, color=(0, 1, 0), scale_factor=0.05)
 
 mlab.plot3d(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2], color=(1, 0, 0), tube_radius=0.01)
 
@@ -77,7 +83,7 @@ plt.ylabel("speed (m/s)")
 plt.legend(["Vx", "Vy", "Vz"])
 plt.grid(True)
 
-plt.subplot(2, 1, 2)
+plt.subplot(2, 2, 3)
 plt.plot(time, abs_speed)
 plt.title("Absolute speed")
 plt.xlabel("time (sec)")
@@ -85,12 +91,14 @@ plt.ylabel("speed (m/s)")
 plt.legend(["V"])
 plt.grid(True)
 
-# plt.subplot(2, 2, 2)
-# plt.plot(time, force)
-# plt.title("Debug Force")
-# plt.xlabel("time (sec)")
-# plt.ylabel("force (N)")
-# plt.legend(["Fx", "Fy", "Fz"])
-# plt.grid(True)
+plt.subplot(2, 2, 4)
+plt.plot(time, Ec)
+plt.plot(time, Ep)
+plt.plot(time, Em)
+plt.title("Energies")
+plt.xlabel("time (sec)")
+plt.ylabel("energy (J)")
+plt.legend(["Ec", "Ep", "Em"])
+plt.grid(True)
 
 plt.show()

surface_guided_sim/surface/eggbox.py
@@ -6,7 +6,7 @@ from typing import Callable
 
 
 class EggBox(Surface):
-    def __init__(self, R: float = 1.0, a: float = 2*np.pi, b: float = 2*np.pi):
+    def __init__(self, R: float = 1.0, a: float = 1.0, b: float = 1.0):
         self.R = R
         self.a = a
         self.b = b
@@ -38,7 +38,7 @@ class EggBox(Surface):
 
             duvx=0,
             duvy=0,
-            duvz=-R*a*b*sinau*sinbv,
+            duvz=R*a*b*sinau*sinbv,
 
             dvvx=0,
             dvvy=0,

surface_guided_sim/surface/plan.py
@@ -13,7 +13,7 @@ class Plan(Surface):
     def from_xz_rotation(angle: float = 0.0) -> Plan:
         return Plan(np.tan(angle), 0.0)
 
-    def __init__(self, a: float, b: float):
+    def __init__(self, a: float = 1.0, b: float = 1.0):
         self.a = a
         self.b = b
 

surface_guided_sim/surface/sphere.py
@@ -33,17 +33,17 @@ class Sphere(Surface):
         dvy = R*sinu*cosv
         dvz = R*sinv
 
-        duux = -R*sinv*cosu
-        duuy = -R*sinu*sinv
+        duux = -x
+        duuy = -y
         duuz = 0.0
 
-        duvx = -R*sinu*cosv
-        duvy = R*cosu*cosv
+        duvx = -dvy
+        duvy = dvx
         duvz = 0.0
 
-        dvvx = -R*sinu*cosv
-        dvvy = -R*sinu*sinv
-        dvvz = R*cosv
+        dvvx = -duy
+        dvvy = -y
+        dvvz = -z
 
         return np.array([
             x, y, z,

surface_guided_sim/surface/surface.py
@@ -109,12 +109,12 @@ class Surface(abc.ABC):
 
     # return rebuild evals for data
     def trajectory(self, uv: np.ndarray) -> np.ndarray:
-        evals = np.zeros((uv.shape[0], 3))
+        traj = np.zeros((uv.shape[0], 3))
 
         for i, (u, v) in enumerate(uv):
-            evals[i] = self.position(self.eval(u, v))
+            traj[i] = self.position(self.eval(u, v))
 
-        return evals
+        return traj
 
     # vectorial speed
     def speed(self, data: np.ndarray) -> np.ndarray:
@@ -148,3 +148,36 @@ class Surface(abc.ABC):
                 mesh[:, i, j] = self.position(self.eval(u, v))
 
         return mesh
+
+    # check integrity
+    def check(self, nu: int = 20, nv: int = 20, tolerance = 1e-7):
+        from . import diff
+
+        emat = diff.get_diff_surface_errors(self, *self.mesh(nu, nv))
+
+        return np.max(emat) <= tolerance
+
+    # check integrity
+    def check_verbose(self, nu: int = 20, nv: int = 20, tolerance = 1e-7):
+        from . import diff
+
+        emat = diff.get_diff_surface_errors(self, *self.mesh(nu, nv))
+
+        elist = np.max(emat, axis=(1, 2))
+
+        index_str = ["xi", "yi", "zi", "duxi", "dvxi", "duyi", "dvyi", "duzi", "dvzi"]
+
+        print("=== Checking surface definition ===")
+        print("U {0} points from {1} to {2}".format(nu, *self.plimits[0]))
+        print("V {0} points from {1} to {2}".format(nv, *self.plimits[1]))
+        print("Tolerance : {0:.1E}".format(tolerance))
+
+        for index in range(duuxi):
+            print("index[{0} : {1}] max err = {2:.1E} [{3}]".format(
+                index, index_str[index], elist[index], elist[index] <= tolerance
+            ))
+
+        return np.max(elist) <= tolerance
+
+
+