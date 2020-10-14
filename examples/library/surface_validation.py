a6cae12df1f001ab57ed67e00dc95dcf0688f130
Adecy <ld.adecy@gmail.com>
2020-10-14 17:11:03 +0200

    dev examples files

--- examples/library/surface_validation.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-from surfaceg.surface import Surface, Sphere, Plan, Tore, EggBox
-
-surface = Sphere()
-
-print(surface.check_verbose())
--- examples/library/surfaces_library_check.py
+++
@@ -1,3 +0,0 @@
-from surfaceg.surface.misc import check_library_surfaces
-
-check_library_surfaces()
