d0c6817c4119502765eec7e9c8ceba6ae972f2da
Adecy <ld.adecy@gmail.com>
2020-10-26 18:43:05 +0100

    add Elliptic and Hyperbolic paraboloids

--- examples/library/reference/paraboloid_example.py
+++ examples/library/reference/paraboloid_example.py
@@ -1,4 +1,4 @@
-from gsurface.surface import Paraboloid
+from gsurface.surface import EllipticParaboloid, HyperbolicParaboloid
 from gsurface.plotter import mayavi_plot_surfaces, SurfacePlot, mlab
 from gsurface.model import SurfaceGuidedMassSystem, build_s0
 from gsurface.forces import Gravity, ViscousFriction, SpringForce, LengthedSpringForce
@@ -7,14 +7,23 @@ from gsurface.plotter.matplotlib import matplotlib_plot_solutions
 
 import numpy as np
 
-para = Paraboloid(1.0, 1.0)
+# choose
+surface_class = EllipticParaboloid  # or EllipticParaboloid
+
+if surface_class is HyperbolicParaboloid:
+    para = HyperbolicParaboloid(1.0, 5).setlims(v_ul=10)
+    s0 = build_s0(u0=0.0, v0=1.0, du0=3)
+else:
+    para = EllipticParaboloid(1.0, 1.0)
+    s0 = build_s0(u0=0.0, v0=1.0, du0=2)
+
 surface = para.build_surface(*para.mesh(50, 50))
 
 print(para, para.check(20, 20))
 
 model = SurfaceGuidedMassSystem(
     surface=para,
-    s0=build_s0(u0=0.0, v0=1.0, du0=2),
+    s0=s0,
     m=1.0,
     forces=[
         Gravity(m=1.0, g=np.array([0.0, 0.0, -2])),
@@ -36,4 +45,4 @@ mayavi_plot_surfaces([
     SurfacePlot(surface, trajectory=trajectory, animate=True)
 ])
 
-mlab.view(0, 30, 5.0)
+mlab.view(0, 30, 5.0, np.zeros((3,)))
--- gsurface/surface/__init__.py
+++ gsurface/surface/__init__.py
@@ -6,8 +6,8 @@ from .sphere import Sphere
 from .eggbox import EggBox
 from .catenoid import Catenoid
 from .conicalcorner import ConicalCorner
-from .paraboloid import Paraboloid
+from .paraboloid import EllipticParaboloid, HyperbolicParaboloid
 
 from .diff import get_diff_surface_errors
 
-__surfaces__ = [Tore, Plan, Sphere, EggBox, Catenoid, ConicalCorner, Paraboloid]
+__surfaces__ = [Tore, Plan, Sphere, EggBox, Catenoid, ConicalCorner, EllipticParaboloid, HyperbolicParaboloid]
