aa26bfa848e025244145aca56b193a777b6a8c7e
adecy <ld.adecy@gmail.com>
2021-01-11 23:22:42 +0100

    add documentation

---
+++ examples/library/dev/num_diff.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def f(x):
+    return np.sin(x)
--- gsurface/serialize/functions.py
+++ gsurface/serialize/functions.py
@@ -1,36 +1,33 @@
-from .interface import SerializableInterface
-
+import base64
+import json
 from typing import List, Dict, Tuple, Union
 
+from .interface import SerializableInterface
 from .json import GSurfaceEncoder, GSurfaceDecoder
-
-import json
-
-import base64
 
 Type = Union[List, Dict, Tuple, SerializableInterface]
 
 
 def save(filename: str, o: Type, indent=None, **kargs):
-    with open(filename, "w+") as fp:
+    with open(filename, "w") as fp:
         json.dump(o, fp, cls=GSurfaceEncoder, indent=indent, **kargs)
 
 
 def load(filename: str, **kargs) -> Type:
-    with open(filename, "r+") as fp:
+    with open(filename, "r") as fp:
         return json.load(fp, cls=GSurfaceDecoder, **kargs)
 
 
-def saveB64(filename: str, o: Type, indent=None, **kargs):
-    raw = json.dumps(o, cls=GSurfaceEncoder, indent=indent, **kargs)
+def saveB64(filename: str, o: Type, **kargs):
+    raw = json.dumps(o, cls=GSurfaceEncoder, indent=None, **kargs)
     b64raw = base64.b64encode(raw.encode("utf8"))
 
-    with open(filename, "bw+") as fp:
+    with open(filename, "bw") as fp:
         fp.write(b64raw)
 
 
 def loadB64(filename: str, **kargs) -> Type:
-    with open(filename, "br+") as fp:
+    with open(filename, "br") as fp:
         b64raw = fp.read()
 
     raw = base64.b64decode(b64raw)
