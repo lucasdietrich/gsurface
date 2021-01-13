0115b4d59fbd210bb769b2d5855bbf27aadfff45
Adecy <ld.adecy@gmail.com>
2021-01-13 20:00:00 +0100

    doc force & surface

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version='0.0.1',
     packages=['gsurface', 'gsurface.forces', 'gsurface.plotter', 'gsurface.surface'],
     url='ldietrich.fr/gsurface',
-    license='',
+    license='GPLv3',
     author='Lucas DIETRICH',
     author_email='pro@ldietrich.fr',
-    python_requires = '>= 3.8'
+    python_requires='>= 3.8'
 )
