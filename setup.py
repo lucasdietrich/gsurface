2f8643a3f3a503294877ccd79e46755aee403f19
Adecy <ld.adecy@gmail.com>
2021-01-13 20:00:00 +0100

    add documentation for force, changed setup.py + version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='gsurface',
-    version='v1.0',
+    description='Simulate surface guided mass points',
+    version='0.0.1',
     packages=['gsurface', 'gsurface.forces', 'gsurface.plotter', 'gsurface.surface'],
     url='ldietrich.fr/gsurface',
     license='',
     author='Lucas DIETRICH',
     author_email='pro@ldietrich.fr',
-    description='Simulate Surface Guided Fall for point masses'
+    python_requires = '>= 3.8'
 )
