9aa313dbe9da87a105b9f9ae9dc5be241a45150d
Adecy <ld.adecy@gmail.com>
2021-02-05 18:43:00 +0100

    updated setup & released 1.0.0dev1

--- setup.py
+++ setup.py
@@ -1,18 +1,7 @@
 import setuptools
 
-with open("readme.md", "r", encoding="utf-8") as fp:
-    long_description = fp.read()
-
 setuptools.setup(
-    name='gsurface',
-    description='Simulate a surface-guided point mass motion according to the Newton 2nd Law Approach',
-    version='0.0.1',
-    author='Lucas DIETRICH',
-    author_email='pro@ldietrich.fr',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/Adecy/gsurface',
-    packages=setuptools.find_packages(),
-    license='GNU General Public License v3 (GPLv3)',
-    python_requires='>= 3.8',
+    packages=setuptools.find_packages(exclude=[
+        "utests*"
+    ]),
 )
